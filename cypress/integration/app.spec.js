58afe71aae8b6aa318130c8167b8f7ba1f273340
Kevin Old <kevin@cypress.io>
2021-04-15 10:57:06 -0500

    update todo item

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -22,7 +22,7 @@
 
 describe('TodoMVC', function () {
   // setup these constants to match what TodoMVC does
-  let TODO_ITEM_ONE = 'buy some cheese'
+  let TODO_ITEM_ONE = 'buy some food'
   let TODO_ITEM_TWO = 'feed the cat'
   let TODO_ITEM_THREE = 'book a doctors appointment'
 
