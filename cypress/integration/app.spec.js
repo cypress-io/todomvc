95431e4048b5968d95ec01b1d71be676272724f5
UsmanGhani-Emumba <usman.ghani@emumba.com>
2021-07-18 16:58:15 +0500

    third flow

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -55,9 +55,7 @@ describe("TodoMVC", function () {
 
   // a very simple example helpful during presentations
   it("adds 2 todos", function () {
-    cy.get(".new-todo")
-      .type("learn testing in Cypress now{enter}")
-      .type("be cool{enter}");
+    cy.get(".new-todo").type("learn testing{enter}").type("be cool{enter}");
 
     cy.get(".todo-list li").should("have.length", 2);
   });
